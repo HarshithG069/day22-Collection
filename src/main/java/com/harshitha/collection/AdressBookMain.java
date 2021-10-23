8e38c90c98a45788096c2b84ff92b97786db6c83
HarshithaG069 <harshithag069@gmail.com>
2021-10-23 13:49:06 +0530

    Added:PersonalContact

--- src/main/java/com/harshitha/collection/AdressBookMain.java
+++ src/main/java/com/harshitha/collection/AdressBookMain.java
@@ -3,6 +3,16 @@ package com.harshitha.collection;
 public class AdressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book");
+        PersonalContacts personalContacts = new PersonalContacts();
+        personalContacts.setFirstName("Harshitha\n");
+        personalContacts.setLastName("G\n");
+        personalContacts.setAddress("Hossur\n");
+        personalContacts.setCity("Chikkaballapur\n");
+        personalContacts.setState("Karnataka\n");
+        personalContacts.setZipCode(561210);
+        personalContacts.setPhoneNumber(8296702715L);
+        personalContacts.setEmail("harshithag069@gmail.com \n");
+        System.out.println("Personal Contacts: " + personalContacts);
     }
 
 }
